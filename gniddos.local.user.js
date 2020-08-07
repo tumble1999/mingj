0d0b1e2c538998c1ae6cb289150b4a9c507de93d
Tumble <tumblegamer@gmail.com>
2020-08-07 13:26:28 +0100

    local version

--- gniddos.local.user.js
+++ gniddos.local.user.js
@@ -7,10 +7,10 @@
 // @require      https://github.com/SArpnt/joinFunction/raw/master/script.js
 // @require      https://github.com/SArpnt/EventHandler/raw/master/script.js
 // @require      https://github.com/SArpnt/cardboard/raw/master/script.user.js
-// @require      https://github.com/boxcrittersmods/critter-bash/raw/master/message.js
-// @require      https://github.com/boxcrittersmods/critter-bash/raw/master/init.js
-// @require      https://github.com/boxcrittersmods/critter-bash/raw/master/bash.js
-// @require      https://github.com/boxcrittersmods/critter-bash/raw/master/commands.js
+// @require      file:///E:/dev/boxcritters/mods/critter-bash/message.js
+// @require      file:///E:/dev/boxcritters/mods/critter-bash/init.js
+// @require      file:///E:/dev/boxcritters/mods/critter-bash/bash.js
+// @require      file:///E:/dev/boxcritters/mods/critter-bash/commands.js
 // @match        https://boxcritters.com/play/
 // @match        https://boxcritters.com/play/?*
 // @match        https://boxcritters.com/play/#*

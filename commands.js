45032ae925846db448701231ae8b50b8a7f2dd3d
Tumble <tumblegamer@gmail.com>
2020-08-07 13:17:53 +0100

    cd does not need to output

--- commands.js
+++ commands.js
@@ -18,7 +18,6 @@ Gniddos.bin.cd = function(args) {
 	var path = args.length>1?args[args.length-1]:"."
 	path = GDgetAbsolutePath(path)
 	Gniddos.etc.cd = path;
-	console.log(Gniddos.etc.cd);
 }
 Gniddos.bin.help = function(args) {
 	Gniddos.etc.path.forEach(p=>Gniddos.bin.dir(["dir",p]));

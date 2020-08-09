0f8ce3e38d9fbec3dda403afcf5119f040fc3c29
Tumble <tumblegamer@gmail.com>
2020-08-09 14:50:04 +0100

    plain prompt

--- web/prompt-style.js
+++ web/prompt-style.js
@@ -1,7 +1,5 @@
-Bash("su mingj")
-
 var prompts = document.getElementsByClassName("prompt");
 for (const prompt of prompts) {
 	var text = prompt.textContent;
-	prompt.textContent = MGJgetPrompt() + text;
+	prompt.textContent = "$ " + text;
 }

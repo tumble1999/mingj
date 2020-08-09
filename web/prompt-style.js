e7933ff682c93fb5142f0c791cfeb6c961652828
Tumble <tumblegamer@gmail.com>
2020-08-09 15:12:20 +0100

    404 page

--- web/prompt-style.js
+++ web/prompt-style.js
@@ -1,5 +1,10 @@
 var prompts = document.getElementsByClassName("prompt");
 for (const prompt of prompts) {
 	var text = prompt.textContent;
+	if(prompt.classList.contains("hostname")) {
+		prompt.textContent = MGJgetPrompt() + text;
+
+	} else {
 	prompt.textContent = "$ " + text;
+	}
 }
